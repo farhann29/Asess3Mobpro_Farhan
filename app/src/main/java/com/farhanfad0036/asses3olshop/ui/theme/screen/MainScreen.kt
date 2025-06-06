d880969500fb386dacdd71091ec49ab5d028f952
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-06-06 22:37:41 +0700

    Membuat indikator loading JSON

--- app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/MainScreen.kt
+++ app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/MainScreen.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,6 +34,7 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.farhanfad0036.asses3olshop.R
 import com.farhanfad0036.asses3olshop.model.Barang
+import com.farhanfad0036.asses3olshop.network.ApiStatus
 import com.farhanfad0036.asses3olshop.network.BarangApi
 import com.farhanfad0036.asses3olshop.ui.theme.theme.Asses3OlshopTheme
 
@@ -52,17 +55,30 @@ fun MainScreen() {
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
+fun ScreenContent(modifier: Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data by viewModel.data
+    val status by viewModel.status.collectAsState()
 
-    LazyVerticalGrid(
-        modifier = modifier.fillMaxSize().padding(4.dp),
-        columns = GridCells.Fixed(2),
-    ) {
-        items(data) { ListItem(barang = it) }
+    when (status) {
+        ApiStatus.LOADING -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        ApiStatus.SUCCESS -> {
+            LazyVerticalGrid(
+                modifier = modifier.fillMaxSize().padding(4.dp),
+                columns = GridCells.Fixed(2),
+            ) {
+                items(data) { ListItem(barang = it) }
+            }
+        }
     }
-
 }
 
 @Composable
--- app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/MainViewModel.kt
+++ app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/MainViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.farhanfad0036.asses3olshop.model.Barang
+import com.farhanfad0036.asses3olshop.network.ApiStatus
 import com.farhanfad0036.asses3olshop.network.BarangApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 
@@ -15,13 +17,18 @@ class MainViewModel : ViewModel() {
     var data = mutableStateOf(emptyList<Barang>())
         private set
 
+    var status = MutableStateFlow(ApiStatus.LOADING)
+        private set
+
     init {
         retrieveData()
     }
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
+            status.value = ApiStatus.LOADING
             try {
                 data.value = BarangApi.service.getBarang()
+                status.value = ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
