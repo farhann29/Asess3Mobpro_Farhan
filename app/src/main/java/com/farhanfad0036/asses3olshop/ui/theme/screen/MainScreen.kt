cdec3e0bcdc8d71212d147f0ab7b162953ab1274
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-06-06 22:25:24 +0700

    Membuat indikator loading image

--- app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/MainScreen.kt
+++ app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/MainScreen.kt
@@ -21,8 +21,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -78,6 +78,7 @@ fun ListItem(barang: Barang) {
                 .build(),
             contentDescription = stringResource(R.string.gambar, barang.nama),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
             modifier = Modifier.fillMaxSize().padding(4.dp)
         )
         Column (
