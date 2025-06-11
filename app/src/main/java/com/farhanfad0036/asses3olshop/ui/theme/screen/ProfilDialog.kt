1cbf05c5ce12b64721b224dbd32bc4939a8299bf
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-06-11 07:16:36 +0700

    Membuat tampilan profil

---
+++ app/src/main/java/com/farhanfad0036/asses3olshop/ui/theme/screen/ProfilDialog.kt
@@ -0,0 +1,97 @@
+package com.farhanfad0036.asses3olshop.ui.theme.screen
+
+import androidx.compose.ui.window.Dialog
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.farhanfad0036.asses3olshop.R
+import com.farhanfad0036.asses3olshop.model.User
+import com.farhanfad0036.asses3olshop.ui.theme.theme.Asses3OlshopTheme
+
+@Composable
+fun ProfilDialog(
+    user: User,
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card (
+            modifier = Modifier.padding(16.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(user.photoUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(id = R.drawable.loading_img),
+                    error = painterResource(id = R.drawable.broken_img),
+                    modifier = Modifier.size(100.dp)
+                )
+                Text(
+                    text = user.name,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+                Text(
+                    text = user.email,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row (
+                    modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton(
+                        onClick = {onDismissRequest() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(stringResource(R.string.tutup))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DialogPreview() {
+    Asses3OlshopTheme {
+        ProfilDialog(
+            user = User("Farhan", "frhn123@gmail.com", ""),
+            onDismissRequest = {},
+            onConfirmation = {}
+        )
+    }
+}
